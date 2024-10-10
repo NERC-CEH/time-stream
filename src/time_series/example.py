ec109ee068e2db89e685022521a3dd3ed5be4b4e
ricsmi <ricsmi@ceh.ac.uk>
2024-10-10 16:20:17 +0100

    Update example.

--- src/time_series/example.py
+++ src/time_series/example.py
@@ -8,14 +8,14 @@ from time_series.period import Period
 
 # Create a DataFrame to simulate random datetime and value data over several years
 # this example simulates AMAX style data, with each year having one value on a random day
-years = range(2010, 2021)
+years = range(1900, 2000)
 timestamps = []
 values = []
 
 # Generating random datetime values within each year
 for year in years:
     day = random.randint(1, 28)
-    month = random.randint(1, 12)
+    month = random.randint(1, 9)
     hour = random.choice(range(0, 23, 9))
     minute = random.choice(range(0, 60, 15))
     second = 0
@@ -23,43 +23,12 @@ for year in years:
     timestamps.append(random_dt)
     values.append(random.uniform(0, 100))  # Random values between 0 and 100
 
-# Convert to a Polars DataFrame
-df = pl.DataFrame({"timestamp": timestamps, "value": values})
-
-# ts = TimeSeries.from_polars(df, "timestamp")
-
-# print(ts)
-
-
-# Create a DataFrame to simulate random datetime and value data over several years
-# this example simulates monthly data
-years = range(2010, 2021)
-timestamps = []
-values = []
-
-# Generating random datetime values within each year
-for year in years:
-    month = random.choice(range(1, 12))
-    day = random.choice(range(1, 28))
-    hour = random.choice(range(0, 23))
-    minute = random.choice(range(0, 60, 15))
-    second = 0
-    dt = datetime(year, month, day, hour, minute, second)
-    timestamps.append(dt)
-    values.append(random.uniform(0, 100))  # Random values between 0 and 100
-
-# timestamps = [
-#     datetime(2000, 10, 1),
-#     datetime(2001, 11, 1, 16),
-#     datetime(2002, 12, 1, 4, 3),
-#     datetime(2004, 1, 1, 7, 10),
-# ]
-# values= [1,2,3,4]
-
-# Convert to a Polars DataFrame
-df = pl.DataFrame({"timestamp": timestamps, "value": values})
-
 resolution = Period.of_minutes(15)
 periodicity = Period.of_years(1).with_month_offset(9).with_hour_offset(9)
 
+# Convert to a Polars DataFrame
+df = pl.DataFrame({"timestamp": timestamps, "value": values})
+
 ts = TimeSeries.from_polars(df, "timestamp", resolution, periodicity)
+
+print(ts)
