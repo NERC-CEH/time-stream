b96756f683d0838dccdeaa8f9842fe408bf53094
ricsmi <ricsmi@ceh.ac.uk>
2025-03-28 13:32:42 +0000

    Alternative pattern for __init__ to handle initial import errors on build.

--- src/time_stream/__init__.py
+++ src/time_stream/__init__.py
@@ -1,12 +1,20 @@
 import autosemver
 
-from time_stream.base import TimeSeries
-from time_stream.period import Period
-
-__all__ = ["TimeSeries", "Period"]
-
-
 try:
     __version__ = autosemver.packaging.get_current_version(project_name="time_stream")
 except Exception:
     __version__ = "0.0.0"
+
+
+__all__ = ["TimeSeries", "Period"]  # noqa
+
+def __getattr__(name):
+    if name == "TimeSeries":
+        from time_stream.base import TimeSeries
+        return TimeSeries
+
+    if name == "Period":
+        from time_stream.period import Period
+        return Period
+
+    raise AttributeError(f"module {__name__} has no attribute {name}")
